176bd28b94a431cc2e53ab9a8a390e4295e50689
KIM-KI-TAE <93kimkt@naver.com>
2015-12-15 03:32:44 +0900

    최종_수정

--- background.py
+++ background.py
@@ -7,23 +7,22 @@ background_y = 0
 background_y_2 = 700
 
 class Background:
+    move_y = 100
     def __init__(self):
         self.image = load_image('C:\\2D\\flight_game_fraemwork\\dragon flight\\dragon flight\\background\\sea.png')
-        self.image2 = load_image('C:\\2D\\flight_game_fraemwork\\dragon flight\\dragon flight\\background\\sea.png')
+        #self.image2 = load_image('C:\\2D\\flight_game_fraemwork\\dragon flight\\dragon flight\\background\\sea.png')
         self.bgm = load_music('C:\\2D\\flight_game_fraemwork\\dragon flight\\dragon flight\\bgm\\dragon_flight2.mp3')
         self.bgm.set_volume(30)
         self.bgm.repeat_play()
+        self.y = 0
 
-    def update(self):
-        global background_y, background_y_2
-        background_y -=5
-        background_y_2 -=5
-        if(background_y_2 == 0):
-            background_y = 0
-            background_y_2 = 700
+    def update(self, frame_time):
+        speed = frame_time * self.move_y
+        self.y += speed
+        if(self.y >= 700) :
+            self.y = 0
+
     def draw(self):
-        global background_y, background_y_2
-        self.image.draw_to_origin(0, background_y, 400,700)
-    def draw2(self):
-        global background_y, background_y_2
-        self.image.draw_to_origin(0, background_y_2, 400,700)
+        self.image.clip_draw(0, int(self.y), 400, int(700-self.y)+1, 200, int((700 - self.y)/2))
+        self.image.clip_draw(0, 0, 400, int(self.y)+1, 200, 700 - int(700 - (700 - self.y))/2)
+
--- background1.py
+++ background1.py
@@ -5,22 +5,21 @@ background1_y = 0
 background1_y_2 = 700
 
 class Background1:
+    move_y = 100
     def __init__(self):
         self.image = load_image('C:\\2D\\flight_game_fraemwork\\dragon flight\\dragon flight\\background\\spring.png')
         self.image2 = load_image('C:\\2D\\flight_game_fraemwork\\dragon flight\\dragon flight\\background\\spring.png')
         self.bgm = load_music('C:\\2D\\flight_game_fraemwork\\dragon flight\\dragon flight\\bgm\\dragon_flight.mp3')
         self.bgm.set_volume(30)
         self.bgm.repeat_play()
-    def update(self):
-        global background1_y, background1_y_2
-        background1_y -=5
-        background1_y_2 -=5
-        if(background1_y_2 == 0):
-            background1_y = 0
-            background1_y_2 = 700
+        self.y = 0
+    def update(self, frame_time):
+        speed = frame_time * self.move_y
+        self.y += speed
+        if(self.y >= 700) :
+            self.y = 0
+
     def draw(self):
-        global background1_y, background1_y_2
-        self.image.draw_to_origin(0, background1_y, 400,700)
-    def draw2(self):
-        global background1_y, background1_y_2
-        self.image.draw_to_origin(0, background1_y_2, 400,700)
+        self.image.clip_draw(0, int(self.y), 400, int(700-self.y)+1, 200, int((700 - self.y)/2))
+        self.image.clip_draw(0, 0, 400, int(self.y)+1, 200, 700 - int(700 - (700 - self.y))/2)
+
